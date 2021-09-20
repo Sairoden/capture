2ab86fb87e42f98f94d781a29272de7313f6bb63
Sairoden <odingandarosa@gmail.com>
2021-09-20 16:42:03 +0800

    Add Styled Component

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background: #1b1b1b;
+
+  }
+
+  button {
+    background: transparent;
+    border: 3px solid #23d997;
+    color: #fff;
+    cursor: pointer;
+    font-size: 1.1.rem;
+    font-weight: bold;
+    padding: 1rem 2rem;
+    transition: all .5s ease;
+
+    &:hover {
+      background: #23d997;
+      color: #fff
+    } 
+  }
+
+  h2 {
+    font-size: 4rem;
+    font-weight: lighter;
+  }
+
+  h3 {
+    color: #fff;
+  }
+
+  h4 {
+    font-weight: bold;
+  }
+
+  a {
+    font-size: 1.1rem;
+  }
+
+  span {
+    color: #23d997;
+    font-weight: bold;
+  }
+
+  p {
+    color: #ccc;
+    font-size: 1.4rem;
+    line-height: 150%;
+    padding: 3rem 0rem;
+  }
+`;
+
+export default GlobalStyle;
