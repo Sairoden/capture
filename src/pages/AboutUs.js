dbe0b145c8bb39d213439470f5baa753cc8cb00e
Sairoden <odingandarosa@gmail.com>
2021-09-20 15:17:58 +0800

    Add Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,15 @@
 // Page Components
+import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection></AboutSection>;
+  return (
+    <React.Fragment>
+      <AboutSection></AboutSection>
+      <ServicesSection></ServicesSection>
+    </React.Fragment>
+  );
 };
 
 export default AboutUs;
