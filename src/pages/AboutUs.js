fccb6711add2176dc2b57e9964b789b99b6bca10
Sairoden <odingandarosa@gmail.com>
2021-09-20 15:29:28 +0800

    ADd Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <React.Fragment>
       <AboutSection></AboutSection>
       <ServicesSection></ServicesSection>
+      <FaqSection></FaqSection>
     </React.Fragment>
   );
 };
