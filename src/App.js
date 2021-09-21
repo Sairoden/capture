d704b36abc9170058b6af6779b9351d44e2f769f
Sairoden <odingandarosa@gmail.com>
2021-09-21 14:46:34 +0800

    Add Nav and Implement React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 // Import Pages
 import AboutUs from "./pages/AboutUs";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import Nav from "./components/Nav";
 import GlobalStyle from "./components/GlobalStyle";
+
+// Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle></GlobalStyle>
-      <AboutUs></AboutUs>
+      <Nav></Nav>
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs></AboutUs>
+        </Route>
+        <Route path="/work">
+          <OurWork></OurWork>
+        </Route>
+        <Route path="/contact">
+          <ContactUs></ContactUs>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <Link id="logo" to="/">
+          Capture
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">1. About Us</Link>
+        </li>
+        <li>
+          <Link to="/work">2. Our Work</Link>
+        </li>
+        <li>
+          <Link to="/contact">3. Contact Us</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.div`
+  align-items: center;
+  background: #282828;
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+  min-height: 10vh;
+  padding: 1rem 10rem;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+
+    li {
+      padding-left: 10rem;
+      position: relative;
+    }
+  }
+
+  #logo {
+    font-family: "Lobster", cursive;
+    font-size: 1.5rem;
+    font-weight: lighter;
+  }
+`;
+
+export default Nav;
