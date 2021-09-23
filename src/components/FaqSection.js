0903fce9ca62831446473158b2a7295cbfa75fda
Sairoden <odingandarosa@gmail.com>
2021-09-23 14:32:59 +0800

    Add Faq Animation

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { About } from "../styles";
+import Toggle from "./Toggle";
+import { AnimateSharedLayout } from "framer-motion";
 
 const FaqSection = () => {
   return (
@@ -7,53 +9,47 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How Do I Start?</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-            vitae.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <AnimateSharedLayout>
+        <Toggle title="How Do I start?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque, vitae.
+            </p>
+          </div>
+        </Toggle>
 
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-            vitae.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        <Toggle title={"Daily Schedule"}>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque, vitae.
+            </p>
+          </div>
+        </Toggle>
 
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-            vitae.
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+        <Toggle title={"Different Payment Methods"}>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque, vitae.
+            </p>
+          </div>
+        </Toggle>
 
-      <div className="question">
-        <h4>What Products do you offer</h4>
-        <div className="answer">
-          <p>Lorem ipsum dolor sit amet.</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-            vitae.
-          </p>
-        </div>
-        <div className="faq-line"></div> 
-      </div>
+        <Toggle title={"What Products do you offer"}>
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Doloremque, vitae.
+            </p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
     </Faq>
   );
 };
