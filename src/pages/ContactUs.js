f18498e6d50400c3b15cf00d540f854c7ee3df66
Sairoden <odingandarosa@gmail.com>
2021-09-25 16:55:52 +0800

    Add animation in contact section

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,19 +1,77 @@
 // Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnim } from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyle
       exit="exit"
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       style={{ background: "#fff" }}
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch</motion.h2>
+        </Hide>
+      </Title>
+      <div>
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle></Circle>
+            <h2>Send us a message</h2>
+          </Social>
+        </Hide>
+
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle></Circle>
+            <h2>Send an email</h2>
+          </Social>
+        </Hide>
+
+        <Hide>
+          <Social variants={titleAnim}>
+            <Circle></Circle>
+            <h2>Social Media</h2>
+          </Social>
+        </Hide>
+      </div>
+    </ContactStyle>
   );
 };
+
+const ContactStyle = styled(motion.div)`
+  color: #353535;
+  min-height: 90vh;
+  padding: 5rem 10rem;
+`;
+
+const Title = styled.div`
+  color: #333;
+  margin-bottom: 4rem;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  background: #353535;
+  border-radius: 50%;
+  height: 3rem;
+  width: 3rem;
+`;
+
+const Social = styled(motion.div)`
+  align-items: center;
+  display: flex;
+
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
